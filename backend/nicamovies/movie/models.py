80f43faed165e43de48755eef4e895d0ac1694f7
Daniel Andrés López Sabogal <andreslop@gmail.com>
2023-05-21 15:11:37 -0500

    add models

--- backend/nicamovies/movie/models.py
+++ backend/nicamovies/movie/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=255)
+    plot = models.TextField()
+
+    class Meta:
+        db_table = 'movies'
+
+    def __str__(self):
+        return self.title
+
+class Rating(models.Model):
+    rating = models.FloatField()
+    comment = models.TextField()
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'ratings'
+
+    def __str__(self):
+        return f'Rating of {self.rating} by {self.user}'
--- backend/nicamovies/user/models.py
+++ backend/nicamovies/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return f'{self.name} {self.lastname}'
