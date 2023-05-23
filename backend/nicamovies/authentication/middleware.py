467913f402de42bdd0c2a4826e59284236e9d172
Daniel Andrés López Sabogal <andreslop@gmail.com>
2023-05-23 03:31:22 -0500

    add bypass limitation not receiving auth header

--- backend/nicamovies/authentication/middleware.py
+++ backend/nicamovies/authentication/middleware.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.urls import resolve
 import environ
 import jwt
+import json
 
 env = environ.Env()
 environ.Env.read_env()
@@ -13,10 +14,10 @@ class AuthenticatedUser():
     def __call__(self, request):
         if self.is_protected_route(request):
             try:
-                token = request.headers.get('Authorization')[7:]
+                token = request.headers.get('Authorization')[7:] if request.headers.get('Authorization') else json.loads(request.body).get('Authorization')[7:]
                 jwt.decode(token, env('SECRET_KEY'), algorithms=['HS256'])
                 return self.get_response(request)
-            except:
+            except Exception as e:
                 return JsonResponse({
                         'status': False,
                         'message': 'Not authorized'
@@ -27,7 +28,6 @@ class AuthenticatedUser():
     def is_protected_route(self, request):
         return (
             (resolve(request.path_info).url_name == 'movies' and request.method != 'GET') or
-            (resolve(request.path_info).url_name == 'users' and request.method != 'GET') or
             (resolve(request.path_info).url_name == 'movie' and request.method != 'GET') or
-            resolve(request.path_info).url_name in ['create_rating', 'rating', 'users', 'user']
+            resolve(request.path_info).url_name in ['create_rating', 'users', 'user']
         )
